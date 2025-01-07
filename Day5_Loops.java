299486f15e9ea6d492af886996a3733ea0454321
manasa165 <2311cs010522@mallareddyuniversity.ac.in>
2025-01-07 20:59:28 +0530

    Printing Of Numbers

---
+++ Day5_Loops.java
@@ -0,0 +1,30 @@
+public class Day5_Loops {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        // count = 0
+        //count = 1
+        //4
+        //count = 2
+        //count = 3
+        //count = 4
+        //Loop
+        //1.initialization
+        //condition
+        //incrementation/decrement
+        for(int count = 0; count <= 4;count++){
+            // count = 1
+            System.out.println(count);
+        }
+        // 0 1 2 3 4
+        for(int i = 20 ; i <= 35; i++){
+            System.out.println(i);
+        }
+    }
+}
